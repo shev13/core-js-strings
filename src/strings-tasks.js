7322ac1adc15f06deb956cced54aa2aa850f67e2
Victor Shevchenko <13shev.js@gmail.com>
2023-12-22 09:03:11 +0300

    feat: implement isString function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -38,8 +38,12 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string') return true;
+
+  if (value instanceof String) return true;
+
+  return false;
 }
 
 /**
