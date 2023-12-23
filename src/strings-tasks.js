017194698e843f1592fee92572e17d79c867aa3d
Victor Shevchenko <13shev.js@gmail.com>
2023-12-24 01:44:26 +0300

    feat: implement formatTime function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -240,8 +240,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minStr = minutes.toString().padStart(2, '0');
+  const secStr = seconds.toString().padStart(2, '0');
+  return `${minStr}:${secStr}`;
 }
 
 /**
