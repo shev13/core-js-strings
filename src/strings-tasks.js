ef3193a3ccef867f01cdbb72bd9e260d085bde7c
Victor Shevchenko <13shev.js@gmail.com>
2023-12-24 01:09:48 +0300

    feat: implement removeLastOccurrences function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -168,8 +168,10 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const index = str.lastIndexOf(value);
+  if (index === -1) return str;
+  return str.substring(0, index) + str.substring(index + value.length);
 }
 
 /**
